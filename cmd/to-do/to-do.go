874b53c57ed6007555ff3e0b4e477093b60fe6e0
Kamalov Kamil <kamil17121998@gmail.com>
2024-01-26 16:17:33 +0300

    configure slog as logger

--- cmd/to-do/to-do.go
+++ cmd/to-do/to-do.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/lallison21/to-do/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting to-go", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// в вермии до 1.21 импортировали как сторонюю библиотеку. В 1.21 использовать log/slog
 	// TODO: init logger: slog
@@ -19,4 +29,25 @@ func main() {
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
